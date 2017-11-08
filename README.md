4e72fb93048295067e021f80b3ec2d0cee978feb
jee yong kim <jeeyongkim@jeeui-MacBook-Pro.local>
2017-11-08 14:59:47 +0900

    done.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # node_web-scraping-imagefiles
-웹페이지 이미지 파일 스크랩핑
+
+Chapter 2. HTML 해석 - 웹 크롤링 테크닉 
+============
+
+## 오류 정정
+1. 
+
+## 사용된 라이브러리
+> - cheerio-httpcli : [https://github.com/ktty1220/cheerio-httpcli]
+> - request : [https://github.com/request/request]
+> - fs : [https://nodejs.org/api/fs.html#fs_whatwg_url_object_support]
+> - url : [https://nodejs.org/api/url.html]
---
+++ dl-image.js
@@ -0,0 +1,34 @@
+'use sctrict';
+
+const client = require('cheerio-httpcli'),
+    request = require('request'),
+    fs = require('fs'),
+    urlType = require('url');
+
+let savedir = __dirname +'/img';
+if(!fs.existsSync(savedir)) {
+    fs.mkdirSync(savedir);
+}
+
+let url = "https://ko.wikipedia.org/wiki/"+encodeURIComponent("강아지");
+console.log("url:: ", url);
+let param = {};
+
+client.fetch(url, param, function(err, $, res){
+    if(err){
+        console.log("error :: ");
+        console.log(err);
+        return;
+    }
+    $("img").each(function(idx){
+        let src = $(this).attr('src');
+
+        src = urlType.resolve(url, src);
+
+        let fname = urlType.parse(src).pathname;
+        fname = savedir + "/" + fname.replace(/[^a-zA-Z0-9\.]+/g, '_');
+
+        request(src).pipe(fs.createWriteStream(fname));
+    });
+
+});
